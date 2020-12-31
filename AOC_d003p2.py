243772733226866f077b41d30d27186d7bfffaf1
Aklim21 <anthonyjim101@gmail.com>
2020-12-31 11:11:32 +0000

    commit to master

--- AOC_d003p2.py
+++ AOC_d003p2.py
@@ -41,6 +41,10 @@ pos = [0,0]
 '''
 function to append each co-ordinate covered by cable to path list
 One function for each direction
+
+Repated code so this can be reduced by introducing two variables 
+- One defines x or y change
+- Second defines +/- direction
 '''
 def addPath_U(c,dir, mag):
     for i in range (0, mag):
@@ -87,7 +91,10 @@ def addMag(c,dir, mag):
 
 for i in getData(0):
     addMag(0,i[0],int(i[1:len(i)]))
-    
+
+'''
+Reset pos for second cable
+'''
 pos = [0,0]
 
 for i in getData(1):
@@ -100,22 +107,22 @@ Generate list to store all co-ordinates where cables intersect.
 crossing = list(set(c0Path).intersection(set(c1Path)))
 
 
-'''
-Get absolute distance from origin and return position.
-'''
-def get_min (results):
-    minDist = 10000
-    matDist = 0
-    for i in results:
-        y = (i[0]**2 + i[1]**2 )**(1/2)
-        if y < minDist :
-            minDist = y
-            pos = i
-            matDist = abs(i[0]) + abs(i[1])
+# '''
+# Get absolute distance from origin and return position.
+# '''
+# def get_min (results):
+#     minDist = 10000
+#     matDist = 0
+#     for i in results:
+#         y = (i[0]**2 + i[1]**2 )**(1/2)
+#         if y < minDist :
+#             minDist = y
+#             pos = i
+#             matDist = abs(i[0]) + abs(i[1])
             
-    print(minDist, "Manhattan Dist: " , matDist, pos)
+#     print(minDist, "Manhattan Dist: " , matDist, pos)
     
-get_min(crossing)
+# get_min(crossing)
 
 
 '''
@@ -125,15 +132,49 @@ So we have:
     all intersections
     
     
-for each co-ordinate :
+for i in cable0:
+    full_mag = 999999999 
+    c0_mag += i_mag 
+    if co-ordinate is within crossing list:
+        for i in cable1:
+            if i is == crossing point:
+                get c1_mag
+                x = c0_mag + c1_mag
+                if x < full_mag:
+                    full_mag = x
+                    
+To get c_mag we need to consider the loop count since each item in the path list increments by one,
+the magnitude of the cable is defined by its position in the path. 
 
-    return index of object that matches co-ordinate for c0,c1
-    NOTE: ensure exception check for duplicates
-    sum indexes to get max circuit length
-    
-    
-    
+But we also need to take into consideration that the first crossing point reached on c0 may not be the shortest over all.
+We will need to hold it in a variable and make a comparison: min_len
+
+We will continue to loop through both paths, eachtime updating min_len if the overall length is shorter.
+
+min_len = 999999
+c0_mag=0
+
+for i in cable0 path:
+    c0_mag += 1
+    if i in crossing point list:
+        c1_mag = 0
+        for j in cable1 path:
+            c1 loop count += 1
+            if j == crossing point:
+                if c0_len + c1_len < min_len:
+                    min_len = c0_mag + c1_mag
+return min_len, c0_mag, c1_mag
+
+        
+
 '''
 
-
+def get_shortest(c0Path,c1Path, crossing):
+    min_len = 999999
+    c0_mag = 0
+    for i in c0Path:
+        c0_mag +=1
+        if i 
+        
+        
 
